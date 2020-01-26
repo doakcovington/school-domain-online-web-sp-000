6ccafd2b35d68ffc13736af525033bf3547c5e1b
Doak Covington <doakcovington@protonmail.com>
2020-01-26 02:37:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,11 +3,9 @@
 class School
   attr_reader :name, :roster
 
-  ROSTER = {}
-
   def initialize(name,roster)
     @name = name
-    @roster = roster
+    @roster = {}
   end
 
 end #end of school class
